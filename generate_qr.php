dfede2344c0f5924d7aba01c941e8b5bb7aa6914
FranklinMchana <88222824+FranklinMchana@users.noreply.github.com>
2025-01-29 17:09:04 +0300

    Update generate_qr.php

--- generate_qr.php
+++ generate_qr.php
@@ -1,15 +1,12 @@
 <?php
 require 'phpqrcode/qrlib.php';
 
-// MikroTik Hotspot Login URL
-$login_url = "http://192.168.88.1/login";
+// MikroTik Hotspot Login Script URL
+$hotspot_url = "http://your-server-ip/autologin.php"; // Change to your server IP
 
-// Generate QR Code pointing to login script
-$qr_data = "$login_url?autologin=true";
+// Generate QR Code pointing to the login script
 $qr_file = 'hotspot_qr.png';
-
-// Generate QR Code
-QRcode::png($qr_data, $qr_file, QR_ECLEVEL_L, 10);
+QRcode::png($hotspot_url, $qr_file, QR_ECLEVEL_L, 10);
 
 // Display the QR Code
 echo "<h2>Scan this QR Code to Connect to MikroTik WiFi:</h2>";
